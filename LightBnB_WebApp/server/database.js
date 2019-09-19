4443c7af86f6353dca7ca92e682be45998d1f938
CameronDunning <dunningcameron@gmail.com>
2019-09-19 17:33:17 +0000

    Refactoring

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,17 +1,10 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 
-// Connect to LightBNB database
-const { Pool } = require("pg");
+// require database
+const pool = require("./db/index");
 
-const pool = new Pool({
-  user: "vagrant",
-  password: "123",
-  host: "localhost",
-  database: "lightbnb"
-});
-
-/// Users
+// Users
 
 /**
  * Get a single user from the database given their email.
@@ -136,11 +129,6 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  // const propertyId = Object.keys(properties).length + 1;
-  // property.id = propertyId;
-  // properties[propertyId] = property;
-  // return Promise.resolve(property);
-
   const queryString = `
     INSERT INTO properties (
       title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms, owner_id)
@@ -164,9 +152,6 @@ const addProperty = function(property) {
     property.number_of_bedrooms,
     property.owner_id
   ];
-  // for (const elem in property) {
-  //   values.push(property[elem]);
-  // }
   console.log(queryString, values);
   return pool.query(queryString, values).then(res => res.rows[0]);
 };
