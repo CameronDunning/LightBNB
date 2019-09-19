5f8f2d1a71a599a82b668a14228f65a225231ea7
CameronDunning <dunningcameron@gmail.com>
2019-09-19 15:56:23 +0000

    Modify search to select city, price and rating

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -92,12 +92,41 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const queryString = `
-  SELECT * FROM properties
-  LIMIT $1
+  let queryParams = [];
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id`;
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += ` WHERE city LIKE $${queryParams.length} `;
+  }
+  if (options.owner_id) {
+    queryParams.push(`%${options.owner_id}%`);
+    queryString += ` WHERE properties.owner_id = $${queryParams.length} `;
+  }
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night}`);
+    queryString += ` WHERE cost_per_night > $${queryParams.length} `;
+  }
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night}`);
+    queryString += ` WHERE cost_per_night < $${queryParams.length} `;
+  }
+  queryString = queryString.replace(/WHERE/gi, "AND").replace(/AND/, "WHERE");
+  queryString += `
+  GROUP BY properties.id`;
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += ` HAVING avg(property_reviews.rating) > $${queryParams.length} `;
+  }
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
   `;
-  const values = [limit];
-  return pool.query(queryString, values).then(res => res.rows);
+  console.log(queryString, queryParams);
+  return pool.query(queryString, queryParams).then(res => res.rows);
 };
 exports.getAllProperties = getAllProperties;
 
